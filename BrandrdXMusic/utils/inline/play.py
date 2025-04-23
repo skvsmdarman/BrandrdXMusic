338dd324c400245678c4be5ab50b022abd0ba5f9
skvsmdarman <136343252+skvsmdarman@users.noreply.github.com>
2025-04-24 03:25:21 +0530

    Update play.py

--- BrandrdXMusic/utils/inline/play.py
+++ BrandrdXMusic/utils/inline/play.py
@@ -68,10 +68,10 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="🥀 ᴏᴡɴᴇʀ 🥀", url=f"https://t.me/BRANDEDKING8"
+                text="🥀 ᴏᴡɴᴇʀ 🥀", url=f"https://t.me/XCid_Kagenou"
             ),
             InlineKeyboardButton(
-                text="🥀 ᴄʜᴀᴛ 🥀", url=f"https://t.me/BRANDED_WORLD"
+                text="🥀 ᴄʜᴀᴛ 🥀", url=f"https://t.me/+7HoS7z6P_mpmYmFl"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
